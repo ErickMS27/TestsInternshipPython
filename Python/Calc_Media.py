929fad180416a7d2ed1bfbc19fcd6a7756666cda
ErickMS27 <erickbackend@gmail.com>
2024-02-15 00:31:03 -0300

    Resolução da Quest 1.0) em Py

---
+++ Python/Calc_Media.py
@@ -0,0 +1,35 @@
+while True:
+    nota_1 = float(input("Digite a sua primeira nota: "))
+    while nota_1 < 0 or nota_1 > 10:
+        print("Nota inválida. Por favor, digite uma nota entre 0 a 10:")
+        nota_1 = float(input("Digite a sua primeira nota novamente: "))
+
+    nota_2 = float(input("\nDigite a sua segunda nota: "))
+    while nota_2 < 0 or nota_2 > 10:
+        print("Nota inválida. Por favor, digite uma nota entre 0 a 10:")
+        nota_2 = float(input("Digite a sua segunda nota novamente: "))
+
+    nota_3 = float(input("\nDigite a sua terceira nota: "))
+    while nota_3 < 0 or nota_3 > 10:
+        print("Nota inválida. Por favor, digite uma nota entre 0 a 10:")
+        nota_3 = float(input("Digite a sua terceira nota novamente: "))
+
+    nota_4 = float(input("\nDigite a sua quarta nota: "))
+    while nota_4 < 0 or nota_4 > 10:
+        print("Nota inválida. Por favor, digite uma quarta entre 0 a 10:")
+        nota_4 = float(input("Digite a sua quarta nota novamente: "))
+
+    media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+    print("\nNotas inseridas:\n")
+    print("Primeira nota:", nota_1)
+    print("Segunda nota:", nota_2)
+    print("Terceira nota:", nota_3)
+    print("Quarta nota:", nota_4)
+
+    print("\nMédia das notas =", media)
+
+    reiniciar = input("\nDeseja reiniciar a aplicação? (Y/N): ")
+    if reiniciar.lower() != 'y':
+        print("Aplicação encerrada.")
+        break
