4d4562124e44db45bdeade79fcbfe32ec42ff3db
ErickMS27 <erickbackend@gmail.com>
2024-02-15 00:37:16 -0300

    Resolução da Quest 1.2) em Py

--- Python/Calc_Media.py
+++ Python/Calc_Media.py
@@ -1,33 +1,38 @@
 while True:
+    
+    notas = []
+    
     nota_1 = float(input("Digite a sua primeira nota: "))
     while nota_1 < 0 or nota_1 > 10:
         print("Nota inválida. Por favor, digite uma nota entre 0 a 10:")
         nota_1 = float(input("Digite a sua primeira nota novamente: "))
+    notas.append(nota_1)
 
     nota_2 = float(input("\nDigite a sua segunda nota: "))
     while nota_2 < 0 or nota_2 > 10:
         print("Nota inválida. Por favor, digite uma nota entre 0 a 10:")
         nota_2 = float(input("Digite a sua segunda nota novamente: "))
+    notas.append(nota_2)
 
     nota_3 = float(input("\nDigite a sua terceira nota: "))
     while nota_3 < 0 or nota_3 > 10:
         print("Nota inválida. Por favor, digite uma nota entre 0 a 10:")
         nota_3 = float(input("Digite a sua terceira nota novamente: "))
+    notas.append(nota_3)
 
     nota_4 = float(input("\nDigite a sua quarta nota: "))
     while nota_4 < 0 or nota_4 > 10:
         print("Nota inválida. Por favor, digite uma quarta entre 0 a 10:")
         nota_4 = float(input("Digite a sua quarta nota novamente: "))
+    notas.append(nota_4)
 
-    media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+    media = sum(notas) / len(notas)
 
     print("\nNotas inseridas:\n")
-    print("Primeira nota:", nota_1)
-    print("Segunda nota:", nota_2)
-    print("Terceira nota:", nota_3)
-    print("Quarta nota:", nota_4)
+    for i, nota in enumerate(notas, start=1):
+        print(f"{i}º Nota: {nota}")
 
-    print("\nMédia das notas =", media)
+    print("\nMédia das notas = ", media)
 
     if media >= 7:
         print("\nVocê está aprovado(a)!")
@@ -37,6 +42,6 @@ while True:
         print("\nVocê está reprovado(a).")
 
     restart = input("\nDeseja reiniciar a aplicação? Se SIM clique Y, caso não queira, clique N:\n")
-    if reiniciar != 'y':
-        print("Aplicação encerrada.")
+    if restart.lower() != 'y':
+        print("\nAplicação encerrada.")
         break
